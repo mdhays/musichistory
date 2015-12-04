64a13fcf09372715a49f5fa456394e12525ddb9c
mdhays <mdhays1995@gmail.com>
2015-12-04 11:38:16 -0600

    Add music button now correctly displays the form partial.

--- app/main.js
+++ app/main.js
@@ -7,13 +7,13 @@ app.config(['$routeProvider',
       when('/songs/list', {
         templateUrl: 'partials/song-list.html',
         controller: 'SongCtrl'
+      }).
+      // Route for the song form to load. It loads when clicking "Add music"
+      when('/songs/new', {
+        templateUrl: 'partials/song-form.html',
+        controller: 'SongCtrl'
       })
-      //Route for the song form to load. It loads when clicking "Add music"
-      // when('/songs/new', {
-      //   templateUrl: 'partials/song-form.html',
-      //   controller: 'SongCtrl'
-      // })
-      //Route for the song list to load if the url doesn't match either of the above urls.
-      //.otherwise
+      // Route for the song list to load if the url doesn't match either of the above urls.
+      // .otherwise
       .otherwise('/songs/list');
   }]);
