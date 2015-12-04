ce53ef86fec6c2e9eca50b59ec38459781a272c7
mdhays <mdhays1995@gmail.com>
2015-12-04 11:01:57 -0600

    Initial commit. Began setting up module songShow to get my firebase data and display it to the dom. Next step is to work on my routes.

---
+++ app/songShow.js
@@ -0,0 +1,16 @@
+var app = angular.module("SongApp", ['firebase', 'ngRoute']);
+
+app.controller("SongCtrl", 
+  ["$scope", 
+   "$firebaseArray", 
+  function($scope, $firebaseArray) {
+
+  // Just like in the RequireJS version of Music History, make a reference
+    var ref = new Firebase("https://musichistory7hays.firebaseio.com/songs");
+
+  // Instead of snapshot.val(), use this syntax to get songs
+    $scope.songs = $firebaseArray(ref);
+    console.log($scope.songs);
+
+  }
+]);
