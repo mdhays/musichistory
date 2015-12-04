a6ed4f63736bd1cb7b3dd55a0065def43c72ed7c
mdhays <mdhays1995@gmail.com>
2015-12-04 13:23:49 -0600

    Dropdowns are now showing the proper information from firebase.

--- app/songStorage.js
+++ app/songStorage.js
@@ -1,31 +1,31 @@
-app.factory("simpleSongs", 
-["$q", "$http",
-function($q, $http) {
+// app.factory("simpleSongs", 
+// ["$q", "$http",
+// function($q, $http) {
 
-  var song_list;
+//   var song_list;
   // Load songs initiates the promise.
-  function loadSongs () {
-    return $q(function(resolve, reject) {
-        $http.get('./json/part5.json')
-        .success(
-          function(objectFromJSONFile) {
+  // function loadSongs () {
+  //   return $q(function(resolve, reject) {
+  //       $http.get('./json/part5.json')
+  //       .success(
+  //         function(objectFromJSONFile) {
             /* 
             Convert Firebase's object of objects into an array of objects, and store it in the private variable
             */
-            song_list = Object.keys(objectFromJSONFile.songs).map(song => objectFromJSONFile.songs[song]);
-            resolve(song_list);
-          }, function(error) {
-            reject(error);
-          }
-        );
-      }); 
-  }
+  //           song_list = Object.keys(objectFromJSONFile.songs).map(song => objectFromJSONFile.songs[song]);
+  //           resolve(song_list);
+  //         }, function(error) {
+  //           reject(error);
+  //         }
+  //       );
+  //     }); 
+  // }
 
   // Store the promise as a private variable
-  var songPromise = loadSongs();
+  // var songPromise = loadSongs();
 
-  return {
-    loadSongs: function () {
+  // return {
+  //   loadSongs: function () {
       /* 
       Return the promise for controllers to listen to. This requires the following
       code inside the controllers
@@ -39,22 +39,22 @@ function($q, $http) {
         }
       );
       */
-      return songPromise;
-    },
-    getSongs: function() {
-      console.log("Factory returning all songs");
-      return song_list;
-    },
-    getSong: function(name) {
-      console.log("Factory returning single song");
-      return song_list.filter(function(song){
-        return song.name === name;
-      })[0];
-    },
-    addSong: function(song) {
-      console.log("Added single song to factory");
-      song_list.push(song);
-      return song_list;
-    }
-  };
-}]);
+//       return songPromise;
+//     },
+//     getSongs: function() {
+//       console.log("Factory returning all songs");
+//       return song_list;
+//     },
+//     getSong: function(name) {
+//       console.log("Factory returning single song");
+//       return song_list.filter(function(song){
+//         return song.name === name;
+//       })[0];
+//     },
+//     addSong: function(song) {
+//       console.log("Added single song to factory");
+//       song_list.push(song);
+//       return song_list;
+//     }
+//   };
+// }]);
